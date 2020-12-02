b0094a4a9d1fa53e9329da93d9d5b6a0d50c2062
VirtosuDan <45829623+VirtosuDan@users.noreply.github.com>
2020-12-02 17:39:16 +0200

    Update AES.java

--- src/com/company/Security/AES.java
+++ src/com/company/Security/AES.java
@@ -1,64 +1,59 @@
 package com.company.Security;
-import javax.crypto.Cipher;
-import javax.crypto.spec.SecretKeySpec;
-import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.Base64;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.spec.SecretKeySpec;
+
 public class AES {
 
-    private static SecretKeySpec secretKey;
-    private static byte[] key;
+    private static SecretKeySpec getSecretKey(String key) {
 
-    public static void setKey(String myKey)
-    {
-        MessageDigest sha = null;
         try {
-            key = myKey.getBytes("UTF-8");
-            sha = MessageDigest.getInstance("SHA-1");
-            key = sha.digest(key);
-            key = Arrays.copyOf(key, 16);
-            secretKey = new SecretKeySpec(key, "AES");
-        }
-        catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
+            MessageDigest sha = MessageDigest.getInstance("SHA-1");
+            byte[] keyBytes = sha.digest(key.getBytes("UTF-8"));
+            keyBytes = Arrays.copyOf(keyBytes, 16);
+            return new SecretKeySpec(keyBytes, "AES");
+        } catch (Exception e) { e.printStackTrace(); }
+
+        return null;
+
     }
 
-    public static String encrypt(String strToEncrypt, String secret)
-    {
-        try
-        {
-            setKey(secret);
+    public static String encrypt(String plaintext, int key) {
+
+        try {
+            SecretKeySpec secretKey = getSecretKey(String.valueOf(key));
             Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
             cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-            return Base64.getEncoder().encodeToString(cipher.doFinal(strToEncrypt.getBytes("UTF-8")));
-        }
-        catch (Exception e)
-        {
-            System.out.println("Error while encrypting: " + e.toString());
-        }
-        return null;
+            return Base64.getEncoder().encodeToString(cipher.doFinal(plaintext.getBytes("UTF-8")));
+        } catch (Exception e) { e.printStackTrace(); }
+
+        return plaintext;
+
     }
 
-    public static String decrypt(String strToDecrypt, String secret)
-    {
-        try
-        {
-            setKey(secret);
-            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
+    public static String decrypt(String ciphertext, int key) {
+
+        try {
+            SecretKeySpec secretKey = getSecretKey(String.valueOf(key));
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
             cipher.init(Cipher.DECRYPT_MODE, secretKey);
-            return new String(cipher.doFinal(Base64.getDecoder().decode(strToDecrypt)));
-        }
-        catch (Exception e)
-        {
-            System.out.println("Error while decrypting: " + e.toString());
-        }
-        return null;
+            return new String(cipher.doFinal(Base64.getDecoder().decode(ciphertext)));
+        } catch (IllegalBlockSizeException e) {
+            System.out.println("decryption failed / wrong key - IllegalBlockSizeException");
+        } catch (BadPaddingException e) {
+            System.out.println("decryption failed / wrong key - BadPaddingException");
+        } catch (InvalidKeyException e) {
+            System.out.println("decryption failed / wrong key - InvalidKeyException");
+        } catch (Exception e) { e.printStackTrace(); }
+
+        return ciphertext;
+
     }
+
 }
